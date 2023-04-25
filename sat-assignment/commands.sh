237c4fe5319b0fbc1beb18ae63e6e6508aa129b8
rohankalbag <kalbag.rohan.r@gmail.com>
2023-04-25 14:05:13 +0530

    added final submission for sat assignment

---
+++ sat-assignment/commands.sh
@@ -0,0 +1,4 @@
+# make sure to source the setup.sh in minisat_for_class before running this
+minisat_static q1b.cnf q1b.result > q1b.txt
+minisat_static q1c.cnf q1c.result > q1c.txt
+minisat_static q2.cnf q2.result > q2.txt
